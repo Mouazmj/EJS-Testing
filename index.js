7f3cf159882ddf80002d4e0acacc005a00ef287e
Mouaz Jamal <mj225ih@student.lnu.se>
2026-01-13 17:43:46 +0100

    I command it to use the ejs for the view engine its the libary to use the html kod in backend when the server sends back the req to the client

--- index.js
+++ index.js
@@ -4,18 +4,26 @@ const app = express()
 
 app.listen(3000)
 
-app.get('', (req, res) => {
+app.set('view engine', 'ejs')
 
+app.get('/', (req, res) => {
+    res.sendFile('./views/index.html', { root: __dirname })
 })
 
-app.get('', (req, res) => {
-
+app.get('/contact-me', (req, res) => {
+    res.sendFile('./views/contact-me.html', { root: __dirname })
 })
 
-app.get('', (req, res) => {
-
+app.get('/about', (req, res) => {
+    res.sendFile('./views/about.html', { root: __dirname })
 })
 
-app.get('', (req, res) => {
 
+app.get('/about-us', (req, res) => {
+    res.redirect('/about')
+})
+
+
+app.use((req, res) => {
+    res.sendStatus(404).sendFile('./views/404.html', { root: __dirname })
 })
